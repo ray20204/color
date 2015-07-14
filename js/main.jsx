574f890555ce85c7e8456f47460fea9a8eef3f2c
Ray Chang <raychang@pixnet.tw>
2015-07-14 16:19:58 +0800

    [modify]independent ListItem

--- js/main.jsx
+++ js/main.jsx
@@ -2,14 +2,8 @@ var originData;
 var CommentForm = React.createClass({
     handleSubmit: function(e) {
         e.preventDefault();
-        //var author = React.findDOMNode(this.refs.author).value.trim();
         var text = React.findDOMNode(this.refs.text).value.trim();
-        //if (!text || !author) {
-            //return;
-            //}
         this.props.onCommentSubmit({text: text});
-        //React.findDOMNode(this.refs.author).value = '';
-        //React.findDOMNode(this.refs.text).value = '';
     },
     render: function() {
         return (
@@ -25,7 +19,7 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
-        getInitialState: function() {
+    getInitialState: function() {
         return {data: []};
     },
     componentDidMount: function() {
@@ -67,24 +61,9 @@ var test;
 var trInfoClass;
 var CommentList = React.createClass({
     render: function() {
-        var commentNodes = this.props.data.map(function(comment, index) {
-            trInfoClass = {'加護病房': 'listitem danger', '死亡': 'listitem danger', '出院': 'listitem success'};
-            return (
-                <tr key={index} className={trInfoClass[comment.即時動向]}>
-                    <td>{comment.編號}</td>
-                    <td>{comment.姓名}</td>
-                    <td>{comment.性別}</td>
-                    <td>{comment.國籍}</td>
-                    <td>{comment.年齡}</td>
-                    <td>{comment.縣市別}</td>
-                    <td>{comment.收治單位}</td>
-                    <td>{comment.檢傷編號}</td>
-                    <td>{comment.醫療檢傷}</td>
-                    <td>{comment.救護檢傷}</td>
-                    <td>{comment.即時動向}</td>
-                    <td>{comment.轉診要求}</td>
-                </tr>
-            );
+        var commentNodes = [];
+        this.props.data.forEach(function(comment, index) {
+            commentNodes.push(<CommentItem item={comment} />);
         });
         test = commentNodes;
         return (
@@ -108,6 +87,36 @@ var CommentList = React.createClass({
                     {commentNodes}
                 </tbody>
             </table>
+        );
+    }
+});
+var CommentItem = React.createClass({
+    colortag: '',
+    setColorTag: function(state) {
+        var trInfoClass = {'加護病房': 'listitem danger', '死亡': 'listitem danger', '出院': 'listitem success'};
+        this.colortag = trInfoClass[state];
+    },
+    render: function() {
+        var comment = this.props.item;
+        this.setColorTag(comment.即時動向);
+        $.each(this.props.item, function(index, value) {
+            //dofilter
+        });
+        return (
+            <tr className={this.colortag}>
+                <td>{comment.編號}</td>
+                <td>{comment.姓名}</td>
+                <td>{comment.性別}</td>
+                <td>{comment.國籍}</td>
+                <td>{comment.年齡}</td>
+                <td>{comment.縣市別}</td>
+                <td>{comment.收治單位}</td>
+                <td>{comment.檢傷編號}</td>
+                <td>{comment.醫療檢傷}</td>
+                <td>{comment.救護檢傷}</td>
+                <td>{comment.即時動向}</td>
+                <td>{comment.轉診要求}</td>
+            </tr>
         );
     }
 });
